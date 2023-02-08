ec8430c590fc212880a6a4ab3159b08d0dcd4d09
ho0527 <chris960527ho@gmail.com>
2023-02-08 09:43:00 +0800

    20230208

--- 52th/senior/52national/TaskC/footer.js
+++ 52th/senior/52national/TaskC/footer.js
@@ -1,5 +1,8 @@
 let link=document.querySelectorAll("#link")
 let onoffbutton=document.querySelectorAll("#onoff")
+let fb=document.querySelectorAll("#fb")
+let twitter=document.querySelectorAll("#twitter")
+let youtube=document.querySelectorAll("#youtube")
 
 link[0].style.display="block"
 
@@ -11,4 +14,16 @@ onoffbutton[0].onclick=function(){
         link[0].style.display="block"
         onoffbutton[0].innerHTML=`關閉footer`
     }
+}
+
+fb[0].onclick=function(){
+    location.href="https://www.facebook.com/"
+}
+
+twitter[0].onclick=function(){
+    location.href="https://www.twitter.com/"
+}
+
+youtube[0].onclick=function(){
+    location.href="https://www.youtube.com/"
 }
