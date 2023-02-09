7515d9c6edd1b4a3627ef9e195dce4fe85213d68
ho0527 <chris960527ho@gmail.com>
2023-02-09 12:57:09 +0800

    20230209

--- 52th/senior/52national/TaskF/p12.php
+++ 52th/senior/52national/TaskF/p12.php
@@ -1,31 +1,32 @@
 <?php
     $memoryBefore=memory_get_usage();
     echo("p12\n");
-    $input_lines=trim(fgets(STDIN));
-    if(1<=$input_lines&&$input_lines<=10){
-        for($i=0;$i<$input_lines;$i=$i+1){
+    $m=trim(fgets(STDIN));
+    if(1<=$m&&$m<=10){
+        for($i=0;$i<$m;$i=$i+1){
             $n=trim(fgets(STDIN));
-            $num=explode(" ",trim(fgets(STDIN)));
             if(1<=$n&&$n<=100){
-                rsort($num);
-                for($j=0;$j<$n;$j++){
-                    if($num[$j]<9){
-                        $num[$j]++;
-                        for($k=$j-1;$k>=0;$k--) {
-                            $num[$k]=0;
-                        }
-                        break;
-                    }
-                }
-                if($j >= $n) {
-                    echo("1");
-                    for($k=0;$k<$n-1;$k++){
-                        echo "0";
-                    }
-                } else {
-                    echo implode("",$num);
-                }
-                echo "\n";
+                $num=explode(" ",trim(fgets(STDIN)));
+                // rsort($num);
+                // echo "\$num ="; print_r($num); echo "\n";
+                // for($j=0;$j<$n;$j++){
+                //     if($num[$j]<9){
+                //         $num[$j]++;
+                //         for($k=$j-1;$k>=0;$k--) {
+                //             $num[$k]=0;
+                //         }
+                //         break;
+                //     }
+                // }
+                // if($j >= $n) {
+                //     echo("1");
+                //     for($k=0;$k<$n-1;$k++){
+                //         echo "0";
+                //     }
+                // } else {
+                //     echo implode("",$num);
+                // }
+                // echo "\n";
             }else{
                 echo("輸入未符合要求");
             }
