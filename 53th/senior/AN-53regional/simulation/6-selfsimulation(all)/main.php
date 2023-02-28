f3f029b19ca79848cc64e68626ce088fa292108b
ho0527 <chris960527ho@gmail.com>
2023-02-28 15:46:35 +0800

    20230228

---
+++ 53th/senior/AN-53regional/simulation/6-selfsimulation(all)/main.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>網站前台登入頁面</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <?php
+        include("link.php");
+        if(!isset($_SESSION["data"])){ header("location:index.php"); }
+    ?>
+</body>
+</html>
---
+++ 53th/senior/AN-53regional/simulation/6-selfsimulation(all)/verify.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>網站前台登入頁面</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <?php
+        include("link.php");
+        if(!isset($_SESSION["data"])){ header("location:index.php"); }
+    ?>
+    <div class="main">
+        <table class="verifytable">
+            <tr>
+                <td class="verifytd"></td>
+                <td class="verifytd"></td>
+            </tr>
+            <tr>
+                <td class="verifytd"></td>
+                <td class="verifytd"></td>
+            </tr>
+        </table>
+        <div>
+            <form action="">
+                <input type="submit" name="logout" value="登出">
+                <input type="button" onclick="location.reload()" value="重設">
+                <input type="button" onclick="check()" value="確定">
+            </form>
+        </div>
+    </div>
+    <script src="verify.js"></script>
+</body>
+</html>
