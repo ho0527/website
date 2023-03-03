1541711adf19a007754bc3b7c87488d4beb4a413
ho0527 <chris960527ho@gmail.com>
2023-03-03 10:56:00 +0800

    20230303

---
+++ 53th/senior/AN-53regional/simulation/8-selfsimulation(M2~M4)/admin.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>咖啡商品管理系統</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <?php
+        include("link.php");
+        if(!isset($_SESSION["data"])){ header("location:index.php"); }
+    ?>
+    <div class="head">
+        <div class="title">咖啡商品管理系統-會員管理</div>
+        <div class="hbutdiv">
+            <input type="button" class="hbut" onclick="location.href='edit.php'" value="新增使用者">
+            <input type="button" class="hbut" onclick="location.href='main.php'" value="首頁">
+            <input type="button" class="hbut" onclick="location.href=''" value="上架商品">
+            <input type="button" class="hbut" onclick="location.href=''" value="查詢">
+            <input type="button" class="hbut selt" onclick="location.href='admin.php'" value="會員管理">
+            <input type="button" class="hbut" onclick="location.href='link.php?logout='" value="登出">
+        </div>
+    </div>
+</body>
+</html>
--- 53th/senior/AN-53regional/simulation/8-selfsimulation(M2~M4)/edit.php
+++ 53th/senior/AN-53regional/simulation/8-selfsimulation(M2~M4)/edit.php
@@ -11,13 +11,13 @@
     <?php
         include("link.php");
         if(!isset($_SESSION["data"])){ header("location:index.php"); }
-        if($_SESSION["permission"]=="管理者"){
+        if(isset($_GET["edit"])){
             ?>
             <div class="head">
-                <div class="title">咖啡商品管理系統-首頁</div>
+                <div class="title">咖啡商品管理系統-編輯使用者</div>
                 <div class="hbutdiv">
-                    <input type="button" class="hbut" onclick="location.href='edit.php'" value="新增使用者">
-                    <input type="button" class="hbut selt" onclick="location.href='main.php'" value="首頁">
+                    <input type="button" class="hbut selt" onclick="location.href='edit.php'" value="編輯使用者">
+                    <input type="button" class="hbut" onclick="location.href='main.php'" value="首頁">
                     <input type="button" class="hbut" onclick="location.href=''" value="上架商品">
                     <input type="button" class="hbut" onclick="location.href=''" value="查詢">
                     <input type="button" class="hbut" onclick="location.href='admin.php'" value="會員管理">
@@ -28,11 +28,13 @@
         }else{
             ?>
             <div class="head">
-                <div class="title">咖啡商品管理系統-首頁</div>
+                <div class="title">咖啡商品管理系統-新增使用者</div>
                 <div class="hbutdiv">
-                    <input type="button" class="hbut selt" onclick="location.href='main.php'" value="首頁">
+                    <input type="button" class="hbut selt" onclick="location.href='edit.php'" value="新增使用者">
+                    <input type="button" class="hbut" onclick="location.href='main.php'" value="首頁">
                     <input type="button" class="hbut" onclick="location.href=''" value="上架商品">
                     <input type="button" class="hbut" onclick="location.href=''" value="查詢">
+                    <input type="button" class="hbut" onclick="location.href='admin.php'" value="會員管理">
                     <input type="button" class="hbut" onclick="location.href='link.php?logout='" value="登出">
                 </div>
             </div>
