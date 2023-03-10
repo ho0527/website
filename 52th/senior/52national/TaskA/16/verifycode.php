fe1583e888b7590ecdc16cd44d4f23865e04124d
ho0527 <chris960527ho@gmail.com>
2023-03-10 23:26:07 +0800

    20230310

--- 52th/senior/52national/TaskA/16/verifycode.php
+++ 52th/senior/52national/TaskA/16/verifycode.php
@@ -1,23 +1,17 @@
 <?php
-    session_start();//開始session
-    $captchacode="";//定訂字串
-    $characters=array_merge(range("A","Z"),range(2,9));//A~Z/2~9
-    for($i=0;$i<4;$i++){//4碼驗證
-        $captchacode=$captchacode.$characters[array_rand($characters)];//將文字加入字串
-    }
-    $_SESSION["captcha_code"]=$captchacode;
-    $canva=imagecreate(150,70);//創建畫布
+    session_start();
+    $str=str_split($_GET["val"]);
+    $canva=imagecreate(250,85);//創建畫布
     imagecolorallocate($canva,255,255,255);//創建背景顏色
     $paint=imagecolorallocate($canva,255,0,0);//創建畫筆顏色
-    $angle=rand(-15,15);//傾斜角度(隨機)
-    $y=rand(25,45);//y軸(隨機)
     $font=__DIR__."/font.TTF";//TTF檔
-    imagettftext($canva,30,$angle,20,$y,$paint,$font,$captchacode);
-    for($i=0;$i<3;$i++){//3條線線
-        $color=imagecolorallocate($canva,rand(0,255),rand(0,255),rand(0,255));//隨機顏色
-        imageline($canva,rand(0,150),rand(0,50),rand(0,150),rand(0,50),$color);//隨機位置(x1,y1,x2,y2)
+    for($i=0;$i<count($str);$i++){//4碼驗證
+        imagettftext($canva,50,rand(-30,30),20+$i*55,65,$paint,$font,$str[$i]);
     }
-    header("Content-Type:image/jpeg");//header宣告為jpeg
+    for($i=0;$i<3;$i++){//3條線線
+        imageline($canva,rand(0,400),rand(0,100),rand(0,400),rand(0,100),imagecolorallocate($canva,rand(0,255),rand(0,255),rand(0,255)));
+    }
+    header("content-Type:image/jpeg");//header宣告為jpeg
     imagejpeg($canva);//創建畫布
     imagedestroy($canva);//清除畫布
 ?>
