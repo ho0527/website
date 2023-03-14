b3b356c6828f8cb390aa3c674f8412dd93079d03
ho0527 <chris960527ho@gmail.com>
2023-03-14 21:11:54 +0800

    20230314

--- 49th/senior/49regional/verifycode copy.php
+++ 49th/senior/49regional/verifycode copy.php
@@ -2,7 +2,7 @@
     $finalStr=$_GET['val'];
     $canva=imagecreate(30,40);
     imagecolorallocate($canva,255,255,255);
-    if($_GET["val"]==" "){
+    if($_GET["val"]=="+"){
         imagettftext($canva,20,0,10,30,(imagecolorallocate($canva,0,0,0)),(__DIR__."/font.TTF"),"+");
     }else{
         imagettftext($canva,20,0,10,30,(imagecolorallocate($canva,0,0,0)),(__DIR__."/font.TTF"),$_GET["val"]);
