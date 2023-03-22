da63a7d2ffe9b2ca773ae7986d188c32f7a2f67d
ho0527 <chris960527ho@gmail.com>
2023-03-22 10:27:06 +0800

    20230322

--- 49th/senior/49regional/verify.js
+++ 49th/senior/49regional/verify.js
@@ -1,22 +1,81 @@
 let verifytd=document.querySelectorAll(".verifytd")
+let show=false
+let click=0
+let tdword1
+let tdword2
+let tdid1
+let tdid2
 
 verifytd.forEach(function(td){
+    td.style.backgroundColor="white"
+    td.style.color="white"
     td.addEventListener("click",function(){
-        tdclick(this.id)
+        td.style.color="black"
+        if(td.style.backgroundColor=="white"){
+            if(tdid1!=this.id){
+                click=click+1
+                if(click==2){
+                    tdword2=this.innerHTML
+                    tdid2=this.id
+                    setTimeout(function(td){
+                        if(tdword1==tdword2){
+                            verifytd[tdid1].style.backgroundColor="yellow"
+                            verifytd[tdid1].style.color="black"
+                            verifytd[tdid2].style.backgroundColor="yellow"
+                            verifytd[tdid2].style.color="black"
+                            if(tdword1=="E"&&tdword2=="E"){
+                                alert("登入成功")
+                                location.href="main.php"
+                            }
+                        }else{
+                            verifytd[tdid1].style.backgroundColor="white"
+                            verifytd[tdid1].style.color="white"
+                            verifytd[tdid2].style.backgroundColor="white"
+                            verifytd[tdid2].style.color="white"
+                        }
+                        tdid1=""
+                        tdid2=""
+                        tdword1=""
+                        tdword2=""
+                        click=0
+                    },200)
+                }else{
+                    tdword1=this.innerHTML
+                    tdid1=this.id
+                }
+            }else{
+                alert("禁止點選同一格")
+            }
+        }else{
+            alert("禁止點選已翻牌的格子")
+        }
     })
 })
 
-function pass(){
-    alert("登入成功")
-    location.href="main.php"
-}
-
-function check(){
-    pass()
-}
-
 function showall(){
-    
+    if(!show){
+        verifytd.forEach(function(td){
+            if(td.style.backgroundColor=="white"){
+                td.style.color="black"
+                td.addEventListener("click",function(){
+                    tdclick(this.id)
+                })
+            }
+        })
+        document.getElementById("open").innerHTML=`全部蓋牌`
+        show=true
+    }else{
+        verifytd.forEach(function(td){
+            if(td.style.backgroundColor=="white"){
+                td.style.color="white"
+                td.addEventListener("click",function(){
+                    tdclick(this.id)
+                })
+            }
+        })
+        document.getElementById("open").innerHTML=`全部翻牌`
+        show=false
+    }
 }
 
 function tdclick(id){
--- 49th/senior/49regional/verify.php
+++ 49th/senior/49regional/verify.php
@@ -8,54 +8,56 @@
     <body>
         <?php
             include("link.php");
-            if(!isset($_SESSION["data"])){ header("location:index.php"); }else{ if($_SESSION["data"]!="a0000"){ header("location:main.php"); } }
+            if(!isset($_SESSION["data"])){ header("location:index.php"); }else{ if($_SESSION["data"]!="a0000"){ header("location:main.php"); } }            
+            $_SESSION["str"]=[];
+            $_SESSION["rand"]=[];
+            while(count($_SESSION["str"])<3){
+                $str=range("A","Z")[rand(0,25)];
+                if($str!="E"){
+                    $_SESSION["str"][]=$str;
+                    for($i=0;$i<(count($_SESSION["str"])-1);$i=$i+1){
+                        if($_SESSION["str"][$i]==$str){
+                            array_pop($_SESSION["str"]);
+                            break;
+                        }
+                    }
+                }
+            }
+            $_SESSION["str"][]="E";
+            for($i=0;$i<4;$i=$i+1){
+                $_SESSION["str"][]=$_SESSION["str"][$i];
+            }
+            while(count($_SESSION["rand"])<8){
+                $rand=rand(0,7);
+                $_SESSION["rand"][]=$rand;
+                for($i=0;$i<(count($_SESSION["rand"])-1);$i=$i+1){
+                    if($_SESSION["rand"][$i]==$rand){
+                        array_pop($_SESSION["rand"]);
+                        break;
+                    }
+                }
+            }
         ?>
         <div class="main">
-            <h1>翻牌配對驗證模組</h1><hr>
-            <?php
-                $_SESSION["str"]=[];
-                $time=0;
-                while(count($_SESSION["str"])<3){
-                    $str=range("A","Z")[rand(0,25)];
-                    $_SESSION["str"][]=$str;
-                    $time=$time+1;
-                    for($i=$time;$i<count($_SESSION["str"]);$i=$i+1){
-                        if($_SESSION["str"][$i]==$str){
-                            array_pop($_SESSION["str"]);
-                        }
-                    }
-                }
-                print_r($_SESSION["str"]);
-                while(count($_SESSION["str"])<3){
-                    $str=range("A","Z")[rand(0,25)];
-                    $_SESSION["str"][]=$str;
-                    $time=$time+1;
-                    for($i=$time;$i<count($_SESSION["str"]);$i=$i+1){
-                        if($_SESSION["str"][$i]==$str){
-                            array_pop($_SESSION["str"]);
-                        }
-                    }
-                }
-                $key1=rand(0,8);
-            ?>
-            <table class="table mag">
+            <h1>電子競技網站管理</h1><hr>
+            <h2 class="mag">翻牌配對驗證模組</h2>
+            <table class="verifytable mag">
                 <tr>
-                    <td class="verifytd"></td>
-                    <td class="verifytd"></td>
-                    <td class="verifytd"></td>
-                    <td class="verifytd"></td>
+                    <td class="verifytd" id="0"><?php echo($_SESSION["str"][$_SESSION["rand"][0]]) ?></td>
+                    <td class="verifytd" id="1"><?php echo($_SESSION["str"][$_SESSION["rand"][1]]) ?></td>
+                    <td class="verifytd" id="2"><?php echo($_SESSION["str"][$_SESSION["rand"][2]]) ?></td>
+                    <td class="verifytd" id="3"><?php echo($_SESSION["str"][$_SESSION["rand"][3]]) ?></td>
                 </tr>
                 <tr>
-                    <td class="verifytd"></td>
-                    <td class="verifytd"></td>
-                    <td class="verifytd"></td>
-                    <td class="verifytd"></td>
+                    <td class="verifytd" id="4"><?php echo($_SESSION["str"][$_SESSION["rand"][4]]) ?></td>
+                    <td class="verifytd" id="5"><?php echo($_SESSION["str"][$_SESSION["rand"][5]]) ?></td>
+                    <td class="verifytd" id="6"><?php echo($_SESSION["str"][$_SESSION["rand"][6]]) ?></td>
+                    <td class="verifytd" id="7"><?php echo($_SESSION["str"][$_SESSION["rand"][7]]) ?></td>
                 </tr>
             </table>
             <input type="button" class="button" onclick="location.href='link.php?logout='" value="登出">
-            <input type="button" class="button" onclick="location.reload()" value="清除">
-            <input type="button" class="button" onclick="showall()" value="全部翻牌">
-            <input type="button" class="button" onclick="check()" value="確定">
+            <input type="button" class="button" onclick="location.reload()" value="重整">
+            <input type="button" class="button" id="open" onclick="showall()" value="全部翻牌">
         </div>
         <script src="verify.js"></script>
     </body>
