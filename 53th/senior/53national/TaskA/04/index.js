87ccd3a05432806b09ecf2d08643866633c0a64c
ho0527 <chris960527ho@gmail.com>
2023-05-31 17:43:06 +0800

    20230531

--- 53th/senior/53national/TaskA/04/index.js
+++ 53th/senior/53national/TaskA/04/index.js
@@ -3,15 +3,29 @@ let time="night"
 document.getElementById("day").onclick=function(){
     if(time=="night"){
         document.getElementById("body").style.backgroundColor="rgb(246, 246, 131)"
-        document.getElementById("moonandsun").src="image/2.png"
-        time="day"
+        document.getElementById("sun").style.display="block"
+        document.getElementById("sun").style.animation="1s up 1"
+        document.getElementById("moon").style.animation="1s down 1"
+        setTimeout(function(){
+            document.getElementById("moon").style.display="none"
+            document.getElementById("sun").style.animation=""
+            document.getElementById("moon").style.animation=""
+            time="day"
+        },900)
     }
 }
 
 document.getElementById("night").onclick=function(){
     if(time=="day"){
         document.getElementById("body").style.backgroundColor="rgb(26, 34, 51)"
-        document.getElementById("moonandsun").src="image/3.png"
-        time="night"
+        document.getElementById("moon").style.display="block"
+        document.getElementById("sun").style.animation="1s down 1"
+        document.getElementById("moon").style.animation="1s up 1"
+        setTimeout(function(){
+            document.getElementById("sun").style.display="none"
+            document.getElementById("sun").style.animation=""
+            document.getElementById("moon").style.animation=""
+            time="night"
+        },900)
     }
 }
