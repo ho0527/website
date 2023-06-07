9770af5deb4c96a41719b0e5ba1deac1aca7e477
chris ho <98445729+ho0527@users.noreply.github.com>
2023-06-07 22:13:29 +0800

    20230607
    worldskill 上傳

---
+++ 52th/worldskill/TaskA/B11/index.js
@@ -0,0 +1,31 @@
+let time="night"
+
+document.getElementById("day").onclick=function(){
+    if(time=="night"){
+        document.getElementById("body").style.backgroundColor="rgb(197, 159, 5)"
+        document.getElementById("sun").style.display="block"
+        document.getElementById("sun").style.animation="1s up 1"
+        document.getElementById("moon").style.animation="1s down 1"
+        setTimeout(function(){
+            document.getElementById("moon").style.display="none"
+            document.getElementById("sun").style.animation=""
+            document.getElementById("moon").style.animation=""
+            time="day"
+        },900)
+    }
+}
+
+document.getElementById("night").onclick=function(){
+    if(time=="day"){
+        document.getElementById("body").style.backgroundColor="rgb(26, 34, 51)"
+        document.getElementById("moon").style.display="block"
+        document.getElementById("sun").style.animation="1s down 1"
+        document.getElementById("moon").style.animation="1s up 1"
+        setTimeout(function(){
+            document.getElementById("sun").style.display="none"
+            document.getElementById("sun").style.animation=""
+            document.getElementById("moon").style.animation=""
+            time="night"
+        },900)
+    }
+}
---
+++ 52th/worldskill/TaskA/C13/index.js
@@ -0,0 +1,43 @@
+let input=document.getElementById("input")
+let show=document.getElementById("show")
+let change=document.getElementById("change")
+
+function inputchange(){
+    let value=input.value
+
+    // 段落和換行格式
+    if(change.value=="ON"){ value=value.replace(/\n/g,"<br>") }
+    else{ value=value.replace(/\n/g,"<br>") }
+
+    // 標題格式
+    value=value.replace(/(#{1,6})\s(.*)<br>/g,function(match,hash,context){ return "<h"+hash.length+">"+context+"</h"+hash.length+">" })
+    value=value.replace(/(#{1,6})\s(.*)/g,function(match,hash,context){ return "<h"+hash.length+">"+context+"</h"+hash.length+">" })
+
+    // 粗體格式
+    value=value.replace(/\*\*(.*?)\*\*/g,"<strong>$1</strong>")
+
+    // 水平規則格式
+    value=value.replace(/---/g,"<hr>")
+
+    // 列表格式
+    value=value.replace(/-\s(.+)<br>/g,"<li>$1</li>")
+    value=value.replace(/-\s(.+)/g,"<li>$1</li>")
+    value=value.replace(/<li>(.*)<\/li>/g,"<ul><li>$1</li></ul>")
+
+    // 圖片格式
+    value=value.replace(/!\[(.*?)\]\((.*?)\)/g,"<img src='$2' alt='$1'>")
+
+    // 連結格式
+    value=value.replace(/\[(.*?)\]\((.*?)\)/g,"<a href='$2'>$1</a>")
+
+    console.log(value)
+    show.innerHTML=value
+}
+
+change.onclick=function(){
+    if(change.value=="ON"){ change.value="OFF" }
+    else{ change.value="ON" }
+    inputchange()
+}
+
+input.addEventListener("input",function(){ inputchange() })
