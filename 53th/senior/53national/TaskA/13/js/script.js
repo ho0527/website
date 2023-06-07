6cf5e4cda6b2d38ceb32c09e3be21c901cdfb7ed
chris ho <98445729+ho0527@users.noreply.github.com>
2023-06-07 21:10:43 +0800

    20230607
    A13製作

--- 53th/senior/53national/TaskA/13/js/script.js
+++ 53th/senior/53national/TaskA/13/js/script.js
@@ -12,19 +12,6 @@ document.querySelectorAll(".card").forEach(function(card){
         card.style.transform="rotate(15deg)"
     })
 
-    document.addEventListener("pointermove",function(event){
-        if(isdrag){
-            offsetx=event.clientX-x
-            offsety=event.clientY-y
-            card.style.left=offsetx+"px"
-            card.style.top=offsety+"px"
-            document.querySelectorAll(".group").forEach(function(groupevent){
-                groupevent.addEventListener("hover",function(){
-                })
-            })
-        }
-    })
-
     document.addEventListener("pointerup",function(){
         if(isdrag){
             isdrag=false
@@ -32,3 +19,38 @@ document.querySelectorAll(".card").forEach(function(card){
         }
     })
 })
+document.querySelectorAll(".card").forEach(function(card) {
+    let isdrag = false;
+    let x;
+    let y;
+    let offsetx = 0;
+    let offsety = 0;
+
+    card.addEventListener("pointerdown", function(event) {
+        isdrag = true;
+        x = event.clientX - offsetx;
+        y = event.clientY - offsety;
+        card.style.transform = "rotate(15deg)";
+    });
+
+    document.addEventListener("pointerup", function() {
+        if (isdrag) {
+            isdrag = false;
+            card.style.transform = "rotate(0deg)";
+        }
+    });
+});
+
+$(function() {
+    $("#sortable1").sortable({
+        connectWith: ".group-sortable"
+    }).disableSelection();
+
+    $("#sortable2").sortable({
+        connectWith: ".group-sortable"
+    }).disableSelection();
+
+    $("#sortable3").sortable({
+        connectWith: ".group-sortable"
+    }).disableSelection();
+});
