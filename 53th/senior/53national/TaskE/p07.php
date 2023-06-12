3a2f6d4a2f51622545faad6fd40fcc953b1cdacd
chris ho <98445729+ho0527@users.noreply.github.com>
2023-06-12 16:58:37 +0800

    20230612
    沒有用的東西

--- 53th/senior/53national/TaskE/p07.php
+++ 53th/senior/53national/TaskE/p07.php
@@ -1,118 +1,132 @@
 <?php
-    $memorybefore=memory_get_usage();
+//     $memorybefore=memory_get_usage();
 
-    echo("p07\n");
+//     echo("p07\n");
 
-    // 讀取起始單字和終止單字
-    $word=explode(" ",trim(fgets(STDIN)));
-    $start=$word[0];
-    $end=$word[1];
+//     // 讀取起始單字和終止單字
+//     $word=explode(" ",trim(fgets(STDIN)));
+//     $start=$word[0];
+//     $end=$word[1];
 
-    // 讀取單字表數量和單字表
-    $n=(int)trim(fgets(STDIN));
-    $data=[];
-    $worddata=[];
+//     // 讀取單字表數量和單字表
+//     $n=(int)trim(fgets(STDIN));
+//     $data=[];
+//     $worddata=[];
 
+//     for($i=0;$i<$n;$i=$i+1){
+//         $data[]=trim(fgets(STDIN));
+//     }
 
-    for($i=0;$i<$n;$i=$i+1){
-        $data[]=trim(fgets(STDIN));
-    }
+//     // $maindata=[];
+//     $ans=0;// 無法轉換到終止單字，返回 0
+//     // 將單字表轉換為集合，以便快速查找
+//     $maindata=array_flip($data);
 
-    // $maindata=[];
-    $ans=0;// 無法轉換到終止單字，返回 0
-    // 將單字表轉換為集合，以便快速查找
-    $maindata=array_flip($data);
+//     // 如果終止單字不在單字表中，無法轉換，返回 0
+//     if(!isset($maindata[$end])){
+//         $ans=0;
+//     }else{
+//         // 初始化佇列，起始單字和其轉換步數
+//         $queue=[[$start,1]];
 
-    // 如果終止單字不在單字表中，無法轉換，返回 0
-    if(!isset($maindata[$end])){
-        $ans=0;
-    }else{
-        // 初始化佇列，起始單字和其轉換步數
-        $queue=[[$start,1]];
+//         // 開始廣度優先搜索
+//         while(!empty($queue)){
+//             [$word,$steps]=array_shift($queue);
 
-        // 開始廣度優先搜索
-        while(!empty($queue)){
-            [$word,$steps]=array_shift($queue);
+//             if($word==$end){
+//                 $ans=$steps; // 找到終止單字，返回步數
+//                 break;
+//             }
 
-            if($word==$end){
-                $ans=$steps; // 找到終止單字，返回步數
-                break;
-            }
+//             // 對於當前單字的每個字母進行替換
+//             for($i=0;$i<strlen($word);$i=$i+1){
+//                 $oldword=$word[$i];
+//                 // 替換為不同的字母
+//                 for($j=0;$j<26;$j=$j+1){
+//                     $newword=chr(ord("a")+$j);
 
-            // 對於當前單字的每個字母進行替換
-            for($i=0;$i<strlen($word);$i=$i+1){
-                $oldword=$word[$i];
-                // 替換為不同的字母
-                for($j=0;$j<26;$j=$j+1){
-                    $newword=chr(ord("a")+$j);
+//                     if($newword!=$oldword){
+//                         // 替換字母
+//                         $newWord=substr_replace($word,$newword,$i,1);
+    
+//                         // 如果新單字在單字表中，將其加入佇列並從單字表中移除
+//                         if(isset($maindata[$newWord])){
+//                             $queue[]=[$newWord,$steps+1];
+//                             unset($maindata[$newWord]);
+//                         }
+//                     } // 跳過相同的字母
+//                 }
+//             }
+//         }
+//     }
 
-                    if($newword==$oldword){ continue; } // 跳過相同的字母
+//     echo($ans.PHP_EOL);
 
-                    // 替換字母
-                    $newWord=substr_replace($word,$newword,$i,1);
+//     // $ans=0;
+//     // // 建立單字圖
+//     // for($i=0;$i<count($data);$i=$i+1){
+//     //     for($j=0;$j<strlen($start);$j=$j+1){
+//     //         $count=0;
+//     //         for($k=0;$k<strlen($data[$i]);$k=$k+1){
+//     //             if($start[$j]!=$data[$i][$k]){
+//     //                 $count=$count+1;
+//     //             }
+//     //         }
+//     //         if($count==1){ $maindata[]=$data[$i];}
+//     //     }
+//     // }
 
-                    // 如果新單字在單字表中，將其加入佇列並從單字表中移除
-                    if(isset($maindata[$newWord])){
-                        $queue[]=[$newWord,$steps+1];
-                        unset($maindata[$newWord]);
-                    }
-                }
-            }
+//     // 輸出結果
+
+//     $memoryafter=memory_get_usage();
+//     $memorydifference=$memoryafter-$memorybefore;
+//     echo("memory used ".($memorydifference/1048576)."MB");
+?>
+<?php
+$memorybefore = memory_get_usage();
+
+echo "p07\n";
+
+// 讀取起始單字和終止單字
+$input = explode(" ", trim(fgets(STDIN)));
+$start = $input[0];
+$end = $input[1];
+
+// 讀取單字表數量和單字表
+$n =  (int)trim(fgets(STDIN));
+$data = [];
+
+for ($i = 0; $i < $n; $i++) {
+    $data[] = trim(fgets(STDIN));
+}
+$ans = 0; // 無法轉換到終止單字，返回 0
+
+// 將所有可能的單字組合成二維陣列
+$combinations = [];
+foreach ($data as $word) {
+    $combination = [$start];
+    for ($i = 0; $i < strlen($word); $i++) {
+        if ($start[$i] != $word[$i]) {
+            $combination[] = substr_replace($start, $word[$i], $i, 1);
         }
-
-        // // 初始化佇列，起始單字和其轉換步數
-        // $queue=[[$start,1]];
-
-        // // 開始廣度優先搜索
-        // while(!empty($queue)){
-        //     [$word,$steps]=array_shift($queue);
-        //     // 對於當前單字的每個字母進行替換
-        //     for($i=0;$i<strlen($word);$i=$i+1){
-        //         $oldword=$word[$i];
-
-        //         // 替換為不同的字母
-        //         for($j=0;$j<26;$j=$j+1){
-        //             $newword=chr(ord("a")+$j);
-
-        //             if($newword==$oldword){ continue;}  // 跳過相同的字母
-
-        //             // 替換字母
-        //             $newWord=substr_replace($word,$newword,$i,1);
-
-        //             // 如果新單字在單字表中，將其加入佇列並從單字表中移除
-        //             if(isset($maindata[$newword])){
-        //                 $queue[]=[$newword,$steps+1];
-        //                 unset($maindata[$newword]);
-        //             }
-
-        //             if($newword == $end){
-        //                 $ans= $steps+1; // 找到終止單字，返回步數
-        //             }
-        //         }
-        //     }
-        // }
-        // echo($ans.PHP_EOL);
     }
+    $combinations[] = $combination;
+}
 
-    echo($ans.PHP_EOL);
+// 比較所有單字鏈的長度，找出最短的
+foreach ($combinations as $combination) {
+    if ($combination[count($combination) - 1] == $end) {
+        $length = count($combination);
+        if ($ans == 0 || $length < $ans) {
+            $ans = $length;
+        }
+    }
+}
 
-    // $ans=ladderLength($start,$end,$data);
-    // // 建立單字圖
-    // for($i=0;$i<count($data);$i=$i+1){
-    //     for($j=0;$j<strlen($start);$j=$j+1){
-    //         $count=0;
-    //         for($k=0;$k<strlen($data[$i]);$k=$k+1){
-    //             if($start[$j]!=$data[$i][$k]){
-    //                 $count=$count+1;
-    //             }
-    //         }
-    //         if($count==1){ $maindata[]=$data[$i];}
-    //     }
-    // }
+print_r ($combinations);
+echo ($ans . PHP_EOL);
 
-    // 輸出結果
-
-    $memoryafter=memory_get_usage();
-    $memorydifference=$memoryafter-$memorybefore;
-    echo("memory used ".($memorydifference/1048576)."MB");
+$memoryafter = memory_get_usage();
+$memorydifference = $memoryafter - $memorybefore;
+echo ("memory used " . ($memorydifference / 1048576) . "MB");
 ?>
