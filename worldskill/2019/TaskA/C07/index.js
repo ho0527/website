d946a72dff1ce4f20f821b3ae2584f03b08ce84b
chris ho <98445729+ho0527@users.noreply.github.com>
2023-07-08 19:41:28 +0800

    feat&Docs: 20230708
    完成2022AC07時鐘製作以及檔案移動及排版

--- worldskill/2019/TaskA/C07/index.js
+++ worldskill/2019/TaskA/C07/index.js
@@ -1,16 +1,27 @@
-function clock() {
+setInterval(function(){
     let date=new Date()
-    let second=date.getSeconds()
-    let minute=date.getMinutes()
-    let hour=date.getHours()%12
+    let datehour=date.getHours()
+    let dateminute=date.getMinutes()
+    let datesecond=date.getSeconds()
 
-    document.getElementById("second").style.transform="rotate("+(6*second)+"deg)"
-    document.getElementById("minute").style.transform="rotate("+(6*minute+second*0.1)+"deg)"
-    document.getElementById("hour").style.transform="rotate("+(30*hour+minute*0.5)+"deg)"
+    let second=((datesecond/60)*360)+90
+    let minute=((dateminute/60)*360)+90+(datesecond/60)*5
+    let hour=((datehour/12)*360)+90+(dateminute/60)*30
+
+    document.getElementById("hour").style.transform="rotate("+hour+"deg)"
+    document.getElementById("minute").style.transform="rotate("+minute+"deg)"
+    document.getElementById("second").style.transform="rotate("+second+"deg)"
+
+    if(second==90){
+        document.getElementById("second").style.transitionDuration="0s"
+    }else{
+        document.getElementById("second").style.transitionDuration="0.1s"
+    }
+},100)
+
+for(let i=1;i<=60;i=i+1){
+    let span=document.createElement("span")
+    span.classList.add("clocktick")
+    span.style.transform="rotate("+(6*i-6)+"deg) translateX(-50%)"
+    document.getElementById("tick").appendChild(span)
 }
-
-// Update the clock hands immediately
-clock()
-
-// Update the clock hands every second
-setInterval(clock,1000)
