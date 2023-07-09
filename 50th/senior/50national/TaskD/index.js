39d9f54425864cdcdcf57b69d27683951d5e35a5
chris ho <98445729+ho0527@users.noreply.github.com>
2023-07-09 22:11:18 +0800

    new: 20230709
    chrisplugin update & pac man 製作

--- 50th/senior/50national/TaskD/index.js
+++ 50th/senior/50national/TaskD/index.js
@@ -1,27 +1,6 @@
-let fontsize=30
-let difficulty="normal"
+let difficulty="normal" // 預設難易度
 
-document.getElementById("sizelow").onclick=function(){
-    if(fontsize>=12){
-        document.querySelectorAll("body")[0].style.fontSize=(fontsize-1)+"px"
-        console.log(" document.querySelectorAll(\"body\")[0].style.fontSize="+ document.querySelectorAll("body")[0].style.fontSize)
-        document.querySelectorAll("input").forEach(function(event){
-            event.style.fontSize=(fontsize-1)+"px"
-        })
-        fontsize=fontsize-1
-    }
-}
-
-document.getElementById("sizeup").onclick=function(){
-    if(fontsize<=50){
-        document.querySelectorAll("body")[0].style.fontSize=fontsize+1+"px"
-        document.querySelectorAll("input").forEach(function(event){
-            event.style.fontSize=fontsize+1+"px"
-        })
-        fontsize=fontsize+1
-    }
-}
-
+// 難易度調整 START
 document.getElementById("easy").onclick=function(){
     document.getElementById("easy").classList.add("selectbutton")
     document.getElementById("normal").classList.remove("selectbutton")
@@ -42,9 +21,16 @@ document.getElementById("hard").onclick=function(){
     document.getElementById("normal").classList.remove("selectbutton")
     difficulty="hard"
 }
+// 難易度調整 END
 
+// 開始遊戲
 document.getElementById("startgame").onclick=function(){
-    localStorage.setItem("difficulty",difficulty)
-    localStorage.setItem("name",document.getElementById("username").value)
-    location.href="main.html"
+    localStorage.setItem("difficulty",difficulty) // 上傳資料
+    localStorage.setItem("name",document.getElementById("username").value) // 上傳資料
+    location.href="main.html" // 導向
+}
+
+// 開始教學
+docgetid("starttutorial").onclick=function(){
+
 }
--- 50th/senior/50national/TaskD/main.js
+++ 50th/senior/50national/TaskD/main.js
@@ -1,3 +1,17 @@
-let name=localStorage.getItem("name")
-let difficulty=localStorage.getItem("difficulty")
+let username=weblsget("name")
+let difficulty=weblsget("difficulty")
+let data=[]
 
+docgetid("difficulty").innerHTML=difficulty // 拿到難易度並顯示
+docgetid("name").innerHTML=username // 拿到名稱並顯示
+
+let ajax=newajax("GET","map.txt")
+
+ajax.onload=function(){
+    tempdata=ajax.responseText.split("\r\n")
+    for(let i=0;i<tempdata.length;i=i+1){
+        tempdata[i].split(",")
+
+    }
+    console.log(data)
+}
