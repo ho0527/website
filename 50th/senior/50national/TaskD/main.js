93a1fca0e232d0552743e24957e30c7c4e2ddfb9
chris ho <98445729+ho0527@users.noreply.github.com>
2023-07-10 19:45:20 +0800

    new&modify: 20230710
    pac man製作及css更新

--- 50th/senior/50national/TaskD/main.js
+++ 50th/senior/50national/TaskD/main.js
@@ -5,15 +5,17 @@ let data=[]
 docgetid("difficulty").innerHTML=difficulty // 拿到難易度並顯示
 docgetid("name").innerHTML=username // 拿到名稱並顯示
 
-let ajax=newajax("GET","map.txt")
+let ajax=newajax("GET","map.txt") // start ajsx
 
 ajax.onload=function(){
-    tempdata=ajax.responseText.split("\r\n")
+    tempdata=ajax.responseText.split("\r\n") // 分隔及讀取檔案
+    // 產出檔案
     for(let i=0;i<tempdata.length;i=i+1){
         data.push([])
         data[i].push(tempdata[i].split(","))
     }
 
+    // 印出檔案
     for(let i=0;i<data.length;i=i+1){
         for(let j=0;j<data[i].length;j=j+1){
             docgetid("app").innerHTML=docgetid("app").innerHTML+`
