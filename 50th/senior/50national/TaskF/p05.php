7b8db7ddb50ad6da65ec7354be511da491dfb125
chris ho <98445729+ho0527@users.noreply.github.com>
2023-07-18 19:31:02 +0800

    new: 20230718

--- 50th/senior/50national/TaskF/p05.php
+++ 50th/senior/50national/TaskF/p05.php
@@ -2,16 +2,41 @@
     $memoryBefore=memory_get_usage();
 
     echo("p05\n");
-    $input1=explode(" ",trim(fgets(STDIN)));//第1矩陣
-    $input2=explode(" ",trim(fgets(STDIN)));//第2矩陣
-    $sum=[];
-    for($i=0;$i<max(count($input1),count($input2));$i=$i+1){//最大值
-        if(isset($input1[$i])){ $sum[]=$input1[$i]; }else{ $sum[]=0; }
-        if(isset($input2[$i])){ $sum[$i]=$sum[$i]+$input2[$i]; }else{ $sum[$i]=$sum[$i]+0; }
+
+    $n=(int)trim(fgets(STDIN));
+    $keylist=[];
+
+    for($i=0;$i<$n;$i=$i+1){
+        $input=explode(" ",trim(fgets(STDIN)));
+        $column=(int)$input[0];
+        $row=(int)$input[1];
+        $keylist[]=$column;
+        if($i==$n-1){ $keylist[]=$row; }
     }
-    echo("output: \n");
-    echo(implode(" ",$sum));
+
+    $n=count($keylist)-1;
+    $data=[];
+    for($i=0;$i<$n;$i=$i+1){
+        $data[$i]=[];
+        for($j=0;$j<$n;$j++){
+            $data[$i][$j]=0;
+        }
+    }
+
+    for($i=2;$i<=$n;$i=$i+1){
+        for($j=0;$j<$n-$i+1;$j=$j+1){
+            $t=$j+$i-1;
+            $data[$j][$t]=PHP_INT_MAX;
+            for($k=$j;$k<$t;$k=$k+1){
+                $cost=$data[$j][$k]+$data[$k+1][$t]+$keylist[$j]*$keylist[$k+1]*$keylist[$t+1];
+                $data[$j][$t]=min($data[$j][$t],$cost);
+            }
+        }
+    }
+
+    print_r($data);
     echo("\n");
+    echo($data[0][$n-1].PHP_EOL);
 
     $memoryAfter=memory_get_usage();
     $memoryDifference=$memoryAfter-$memoryBefore;
