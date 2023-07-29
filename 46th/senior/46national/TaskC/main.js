3538dd31ff65dd80a200fbffb7b1686d668fa554
chris ho <98445729+ho0527@users.noreply.github.com>
2023-07-29 19:23:05 +0800

    new&modify: 20230729
    - 俄羅斯方塊
    - js to ts

--- 46th/senior/46national/TaskC/main.js
+++ 46th/senior/46national/TaskC/main.js
@@ -1,26 +1,115 @@
-let difficulty=weblsget("difficulty")
-let timemin=0
-let timesec=0
-
-docgetid("difficulty").innerHTML=difficulty // 拿到難易度並顯示
-
-setInterval(function(){
-    timesec=timesec+1
-    if(timesec==60){
-        timemin=timemin+1
-        timesec=0
+var difficulty = weblsget("difficulty");
+var timemin = 0;
+var timesec = 0;
+var width = innerWidth * 0.6;
+var height = innerHeight * 0.85;
+var nowarray = [];
+var blocklist = [];
+var blockarray = [];
+for (var i = 0; i < 17; i = i + 1) {
+    nowarray.push([]);
+    for (var j = 0; j < 10; j = j + 1) {
+        nowarray[i].push(0);
     }
-    let min=timemin.toString()
-    let sec=timesec.toString()
-    if(timesec<10){
-        sec="0"+sec
+}
+docgetid("main").style.width = width + "px";
+docgetid("main").style.height = height + "px";
+docgetid("difficulty").innerHTML = difficulty;
+setInterval(function () {
+    timesec = timesec + 1;
+    if (timesec == 60) {
+        timemin = timemin + 1;
+        timesec = 0;
     }
-    if(timemin<10){
-        min="0"+min
+    var min = timemin.toString();
+    var sec = timesec.toString();
+    if (timesec < 10) {
+        sec = "0" + sec;
     }
-    docgetid("time").innerHTML=`
-        時間: ${min}:${sec}
-    `
-},1000)
-
-startmacossection()
+    if (timemin < 10) {
+        min = "0" + min;
+    }
+    docgetid("time").innerHTML = "\n        \u6642\u9593: ".concat(min, ":").concat(sec, "\n    ");
+}, 1000);
+function block(key) {
+    if (key == "a") {
+        var div = doccreate("div");
+        div.classList.add("typea");
+        docappendchild("main", div);
+        blockarray = [
+            [0, 0, 0, 0],
+            [1, 1, 1, 1]
+        ];
+    }
+    else if (key == "b") {
+        var div = doccreate("div");
+        div.classList.add("typeb");
+        docappendchild("main", div);
+        blockarray = [
+            [1, 0, 0, 0],
+            [1, 1, 1, 0]
+        ];
+    }
+    else if (key == "c") {
+        var div = doccreate("div");
+        div.classList.add("typec");
+        docappendchild("main", div);
+        blockarray = [
+            [0, 0, 1, 0],
+            [1, 1, 1, 0]
+        ];
+    }
+    else if (key == "d") {
+        var div = doccreate("div");
+        div.classList.add("typed");
+        docappendchild("main", div);
+        blockarray = [
+            [0, 1, 1, 0],
+            [0, 1, 1, 0]
+        ];
+    }
+    else if (key == "e") {
+        var div = doccreate("div");
+        div.classList.add("typee");
+        docappendchild("main", div);
+        blockarray = [
+            [0, 1, 1, 0],
+            [1, 1, 0, 0]
+        ];
+    }
+    else if (key == "f") {
+        var div = doccreate("div");
+        div.classList.add("typef");
+        docappendchild("main", div);
+        blockarray = [
+            [0, 1, 0, 0],
+            [1, 1, 1, 0]
+        ];
+    }
+    else if (key == "g") {
+        var div = doccreate("div");
+        div.classList.add("typeg");
+        docappendchild("main", div);
+        blockarray = [
+            [1, 1, 0, 0],
+            [0, 1, 1, 0]
+        ];
+    }
+    else {
+        conlog("[ERROR]error key", "red", "15");
+    }
+}
+function test(key) {
+    if (key) {
+        conlog("teststart", "green", "15");
+        block("a");
+        block("b");
+        block("c");
+        block("d");
+        block("e");
+        block("f");
+        block("g");
+    }
+}
+test(false);
+startmacossection();
--- 46th/senior/46national/TaskD/index.js
+++ 46th/senior/46national/TaskD/index.js
@@ -1,102 +1,84 @@
-let traintypedata
-let stationdata
-let traindata
-let station="<option value='all'>所有</option>"
-let traintype="<option value='all'>所有</option>"
-
-newajax("GET","api.php?traintypelist=").onload=function(){ traintypedata=JSON.parse(this.responseText) }
-newajax("GET","api.php?stationlist=").onload=function(){ stationdata=JSON.parse(this.responseText) }
-newajax("GET","api.php?trainlist=").onload=function(){ traindata=JSON.parse(this.responseText) }
-
-setTimeout(function(){
-    for(let i=0;i<stationdata.length;i=i+1){
-        station=station+`<option value="${stationdata[i][1]}">${stationdata[i][2]}</option>`
+var traintypedata;
+var stationdata;
+var traindata;
+var station = "<option value='all'>所有</option>";
+var traintype = "<option value='all'>所有</option>";
+newajax("GET", "api.php?traintypelist=").onload = function () { traintypedata = JSON.parse(this.responseText); };
+newajax("GET", "api.php?stationlist=").onload = function () { stationdata = JSON.parse(this.responseText); };
+newajax("GET", "api.php?trainlist=").onload = function () { traindata = JSON.parse(this.responseText); };
+setTimeout(function () {
+    for (var i = 0; i < stationdata.length; i = i + 1) {
+        station = station + "<option value=\"".concat(stationdata[i][1], "\">").concat(stationdata[i][2], "</option>");
     }
-
-    for(let i=0;i<traintypedata.length;i=i+1){
-        traintype=traintype+`<option value="${traintypedata[i][0]}">${traintypedata[i][1]}</option>`
+    for (var i = 0; i < traintypedata.length; i = i + 1) {
+        traintype = traintype + "<option value=\"".concat(traintypedata[i][0], "\">").concat(traintypedata[i][1], "</option>");
     }
-
-    docgetid("start").innerHTML=station
-    docgetid("end").innerHTML=station
-    docgetid("traintype").innerHTML=traintype
-    docgetid("date")
-
-    docgetid("submit").onclick=function(){
-        docgetid("start")
-        docgetid("end")
-        docgetid("traintype")
-        docgetid("date")
-    }
-
-    startmacossection()
-},100)
-
-setTimeout(function(){
-    newajax("GET","api.php?trainlist=").onload=function(){
-        let data=JSON.parse(this.responseText)
-        let trainlist=data[0]
-        let stoplist=data[1]
-        let stoplistfilterdata=[]
-        for(let i=0;i<trainlist.length;i=i+1){
-            let id=trainlist[i][0]
-            let stoplistfilter=stoplist.filter(function(event){ return event[1]==id }) // 拿到sotplist的trainid與id相同的數量
-            let traintypename=""
-            let stationname=""
-            let week="";
-
-            for(let j=0;j<traintypedata.length;j=j+1){
-                if(traintypedata[j][0]==trainlist[i][1]){
-                    traintypename=traintypedata[j][1]
+    docgetid("start").innerHTML = station;
+    docgetid("end").innerHTML = station;
+    docgetid("traintype").innerHTML = traintype;
+    docgetid("date");
+    docgetid("submit").onclick = function () {
+        docgetid("start");
+        docgetid("end");
+        docgetid("traintype");
+        docgetid("date");
+    };
+    startmacossection();
+}, 100);
+setTimeout(function () {
+    newajax("GET", "api.php?trainlist=").onload = function () {
+        var data = JSON.parse(this.responseText);
+        var trainlist = data[0];
+        var stoplist = data[1];
+        var stoplistfilterdata = [];
+        var _loop_1 = function (i) {
+            var id = trainlist[i][0];
+            var stoplistfilter = stoplist.filter(function (event) { return event[1] == id; });
+            var traintypename = "";
+            var stationname = "";
+            var week = "";
+            for (var j = 0; j < traintypedata.length; j = j + 1) {
+                if (traintypedata[j][0] == trainlist[i][1]) {
+                    traintypename = traintypedata[j][1];
                 }
             }
-
-            for(let j=0;j<stationdata.length;j=j+1){
-                if(stationdata[j][0]==stoplistfilter[0][2]){
-                    stationname=stationdata[j][2]
+            for (var j = 0; j < stationdata.length; j = j + 1) {
+                if (stationdata[j][0] == stoplistfilter[0][2]) {
+                    stationname = stationdata[j][2];
                 }
             }
-
-            stoplistfilterdata.push(stoplistfilter)
-
-            if(trainlist[i][3]=="1"){
-                week="一"
-            }else if(trainlist[i][3]=="2"){
-                week="二"
-            }else if(trainlist[i][3]=="3"){
-                week="三"
-            }else if(trainlist[i][3]=="4"){
-                week="四"
-            }else if(trainlist[i][3]=="5"){
-                week="五"
-            }else if(trainlist[i][3]=="6"){
-                week="六"
-            }else{
-                week="日"
+            stoplistfilterdata.push(stoplistfilter);
+            if (trainlist[i][3] == "1") {
+                week = "一";
             }
-
-            let tr=doccreate("tr")
-            tr.innerHTML=`
-                <td class="td">${traintypename}</td>
-                <td class="td">${trainlist[i][2]}</td>
-                <td class="td">發車站</td>
-                <td class="td">終點站</td>
-                <td class="td">預計發車時間</td>
-                <td class="td">預計到達時間</td>
-                <td class="td">行駛時間</td>
-                <td class="td">票價</td>
-                <td class="td">
-                    <input type="button" class="bluebutton ticketbutton" value="訂票">
-                </td>
-            `
-            docappendchild("table",tr)
+            else if (trainlist[i][3] == "2") {
+                week = "二";
+            }
+            else if (trainlist[i][3] == "3") {
+                week = "三";
+            }
+            else if (trainlist[i][3] == "4") {
+                week = "四";
+            }
+            else if (trainlist[i][3] == "5") {
+                week = "五";
+            }
+            else if (trainlist[i][3] == "6") {
+                week = "六";
+            }
+            else {
+                week = "日";
+            }
+            var tr = doccreate("tr");
+            tr.innerHTML = "\n                <td class=\"td\">".concat(traintypename, "</td>\n                <td class=\"td\">").concat(trainlist[i][2], "</td>\n                <td class=\"td\">\u767C\u8ECA\u7AD9</td>\n                <td class=\"td\">\u7D42\u9EDE\u7AD9</td>\n                <td class=\"td\">\u9810\u8A08\u767C\u8ECA\u6642\u9593</td>\n                <td class=\"td\">\u9810\u8A08\u5230\u9054\u6642\u9593</td>\n                <td class=\"td\">\u884C\u99DB\u6642\u9593</td>\n                <td class=\"td\">\u7968\u50F9</td>\n                <td class=\"td\">\n                    <input type=\"button\" class=\"bluebutton ticketbutton\" value=\"\u8A02\u7968\">\n                </td>\n            ");
+            docappendchild("table", tr);
+        };
+        for (var i = 0; i < trainlist.length; i = i + 1) {
+            _loop_1(i);
         }
-
-        docgetall(".ticketbutton").forEach(function(event){
-            event.onclick=function(){  }
-        })
-    }
-},100)
-
-startmacossection()
-//<div class="warning">查無相關車輛，請重新搜尋</div>
+        docgetall(".ticketbutton").forEach(function (event) {
+            event.onclick = function () { };
+        });
+    };
+}, 100);
+startmacossection();
