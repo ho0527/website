c9815f66030bdb07b28c9ad81184a0db57c88f8e
chris ho <98445729+ho0527@users.noreply.github.com>
2023-07-30 23:17:08 +0800

    new: 20230730
    46nTaskD

--- 46th/senior/46national/TaskC/main.js
+++ 46th/senior/46national/TaskC/main.js
@@ -3,15 +3,26 @@ let timemin=0
 let timesec=0
 let width=innerWidth*0.6
 let height=innerHeight*0.85
-let nowarray=[]
-let blocklist=[]
-let blockarray=[]
+let nowarray=[] // 現在的狀態
+let blocklist=[] // 方塊列表(a~e)
+let blockarray=[] //
+let temparray // 暫存arr
+let blocklength=20 // 方怪長度
+let rotatedeg=0 // 旋轉度數(0,90,180,270)
+let speed // 移動速度
 
 function block(key){
+    let div=doccreate("div")
+    div.classList.add("grid")
+    div.style.width=(blocklength*4)+"px"
+    div.style.height=(blocklength*4)+"px"
     if(key=="a"){
-        let div=doccreate("div")
-        div.classList.add("typea")
-        docappendchild("main",div)
+        div.innerHTML=`
+            <div class="typea1"></div>
+            <div class="typea2"></div>
+            <div class="typea3"></div>
+            <div class="typea4"></div>
+        `
         blockarray=[
             [0,0,0,0],
             [0,0,0,0],
@@ -19,9 +30,6 @@ function block(key){
             [0,0,0,0]
         ]
     }else if(key=="b"){
-        let div=doccreate("div")
-        div.classList.add("typeb")
-        docappendchild("main",div)
         blockarray=[
             [0,0,0,0],
             [0,1,0,0],
@@ -29,9 +37,6 @@ function block(key){
             [0,0,0,0]
         ]
     }else if(key=="c"){
-        let div=doccreate("div")
-        div.classList.add("typec")
-        docappendchild("main",div)
         blockarray=[
             [0,0,0,0],
             [0,0,0,1],
@@ -39,9 +44,6 @@ function block(key){
             [0,0,0,0]
         ]
     }else if(key=="d"){
-        let div=doccreate("div")
-        div.classList.add("typed")
-        docappendchild("main",div)
         blockarray=[
             [0,0,0,0],
             [0,1,1,0],
@@ -49,9 +51,6 @@ function block(key){
             [0,0,0,0]
         ]
     }else if(key=="e"){
-        let div=doccreate("div")
-        div.classList.add("typee")
-        docappendchild("main",div)
         blockarray=[
             [0,0,0,0],
             [0,0,1,1],
@@ -59,9 +58,6 @@ function block(key){
             [0,0,0,0]
         ]
     }else if(key=="f"){
-        let div=doccreate("div")
-        div.classList.add("typef")
-        docappendchild("main",div)
         blockarray=[
             [0,0,0,0],
             [0,0,1,0],
@@ -69,9 +65,6 @@ function block(key){
             [0,0,0,0]
         ]
     }else if(key=="g"){
-        let div=doccreate("div")
-        div.classList.add("typeg")
-        docappendchild("main",div)
         blockarray=[
             [0,0,0,0],
             [0,1,1,0],
@@ -79,6 +72,7 @@ function block(key){
             [0,0,0,0]
         ]
     }else{ conlog("[ERROR]error key","red","15") }
+    docappendchild("main",div)
 }
 
 function test(key){
@@ -104,6 +98,7 @@ function rotate(){
 }
 
 function down(){
+    speed=0.1
 }
 
 function downtobottom(){
@@ -112,25 +107,28 @@ function downtobottom(){
 function share(){
 }
 
-function stop(){
-    document.addEventListener("keydown",function(event){
-        if(event.key=="ArrowLeft"){ event.preventDefault() }
-        if(event.key=="ArrowRight"){ event.preventDefault() }
-        if(event.key=="ArrowUp"){ event.preventDefault() }
-        if(event.key=="ArrowDown"){ event.preventDefault() }
-        if(event.key==" "){ event.preventDefault() }
-    })
+function stopstart(){
+    if(docgetid("stop").value=="暫停遊戲"){
+        document.addEventListener("keydown",function(event){
+            if(event.key=="ArrowLeft"){ event.preventDefault() }
+            if(event.key=="ArrowRight"){ event.preventDefault() }
+            if(event.key=="ArrowUp"){ event.preventDefault() }
+            if(event.key=="ArrowDown"){ event.preventDefault() }
+            if(event.key==" "){ event.preventDefault() }
+        })
+        docgetid("stop").value="繼續遊戲"
+    }else{
+        document.addEventListener("keydown",function(event){
+            if(event.key=="ArrowLeft"){ event.preventDefault() }
+            if(event.key=="ArrowRight"){ event.preventDefault() }
+            if(event.key=="ArrowUp"){ event.preventDefault() }
+            if(event.key=="ArrowDown"){ event.preventDefault() }
+            if(event.key==" "){ event.preventDefault() }
+        })
+        docgetid("stop").value="暫停遊戲"
+    }
 }
 
-function start(){
-    document.addEventListener("keydown",function(event){
-        if(event.key=="ArrowLeft"){ event.preventDefault();left() }
-        if(event.key=="ArrowRight"){ event.preventDefault();right() }
-        if(event.key=="ArrowUp"){ event.preventDefault();rotate() }
-        if(event.key=="ArrowDown"){ event.preventDefault();down() }
-        if(event.key==" "){ event.preventDefault();downtobottom() }
-    })
-}
 
 function cancel(){
     stop()
@@ -147,6 +145,7 @@ for(let i=0;i<17;i=i+1){
         nowarray[i].push(0)
     }
 }
+temparray=nowarray
 
 docgetid("main").style.width=width+"px"
 docgetid("main").style.height=height+"px"
@@ -172,37 +171,33 @@ setInterval(function(){
 },1000)
 
 document.addEventListener("keydown",function(event){
+    if(event.key=="Escape"){ event.preventDefault();cancel() }
     if(event.key=="ArrowLeft"){ event.preventDefault();left() }
     if(event.key=="ArrowRight"){ event.preventDefault();right() }
     if(event.key=="ArrowUp"){ event.preventDefault();rotate() }
     if(event.key=="ArrowDown"){ event.preventDefault();down() }
     if(event.key==" "){ event.preventDefault();downtobottom() }
-    if(event.key=="Escape"){ event.preventDefault();cancel() }
+    if(event.key=="s"){ event.preventDefault();share() }
+    if(event.key=="p"){ event.preventDefault();stopstart() }
 })
+
+if(difficulty=="normal"){
+    speed=1
+}else{
+    speed=0.25
+}
 
 // 顯示下一個方塊
 docgetid("shownext")
 
 // 分享
-docgetid("share").onclick=function(){
-    share()
-}
+docgetid("share").onclick=function(){ share() }
 
 // 暫停/開始遊戲
-docgetid("stop").onclick=function(){
-    if(docgetid("stop").value=="暫停遊戲"){
-        stop()
-        docgetid("stop").value="繼續遊戲"
-    }else{
-        start()
-        docgetid("stop").value="暫停遊戲"
-    }
-}
+docgetid("stop").onclick=function(){ stopstart() }
 
 // 放棄遊戲
-docgetid("cancel").onclick=function(){
-    cancel()
-}
+docgetid("cancel").onclick=function(){ cancel() }
 
 test(false)
 startmacossection()
