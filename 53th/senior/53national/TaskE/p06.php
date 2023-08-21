52bdda80f12b3b9d66f0e34d880fe28f45375a06
chris ho <98445729+ho0527@users.noreply.github.com>
2023-08-21 22:31:57 +0800

    modify: 20230821
    - 製作53nE-6(未完成)

--- 53th/senior/53national/TaskE/p06.php
+++ 53th/senior/53national/TaskE/p06.php
@@ -7,44 +7,53 @@
     $operand=[];
     $maindata=explode(" ",$data);
 
-    for($i=0;$i<count($maindata);$i=$i+1){
+    for($i=count($maindata)-1;$i>=0;$i--){
         if(in_array($maindata[$i],["+","-","*","/","^"])){
             $datacheck=-1;
-            $enddatacheck=-1;
+            $begindatacheck=-1;
 
             if($maindata[$i]=="+"||$maindata[$i]=="-"){ $datacheck=2; }
             elseif($maindata[$i]=="*"||$maindata[$i]=="/"){ $datacheck=1; }
             elseif($maindata[$i]=="^"){ $datacheck=0; }
 
             if(!empty($operand)){
-                if($operand[count($operand)-1]=="+"||$operand[count($operand)-1]=="-"){ $enddatacheck=0; }
-                elseif($operand[count($operand)-1]=="*"||$operand[count($operand)-1]=="/"){ $enddatacheck=1; }
+                if($operand[0]=="+"||$operand[0]=="-"){ $begindatacheck=2; }
+                elseif($operand[0]=="*"||$operand[0]=="/"){ $begindatacheck=1; }
+                elseif($operand[0]=="^"){ $begindatacheck=0; }
             }
 
-            while(!empty($operand)&&in_array($operand[count($operand)-1],["+","-","*","/"])&&($datacheck<=$enddatacheck)){
-                $rpn[]=array_pop($operand);
+            while(!empty($operand)&&in_array($operand[0],["+","-","*","/","^"])&&($datacheck>=$begindatacheck)){
+                array_unshift($rpn,array_shift($operand));
             }
-            $operand[]=$maindata[$i];
-        }elseif($maindata[$i]=="("){
-            $operand[]=$maindata[$i];
+            array_unshift($operand,$maindata[$i]);
         }elseif($maindata[$i]==")"){
-            while($operand[count($operand)-1]!="("){
-                $rpn[]=array_pop($operand);
+            array_unshift($operand,$maindata[$i]);
+        }elseif($maindata[$i]=="("){
+            while($operand[0]!=")"){
+                array_unshift($rpn,array_shift($operand));
             }
-            array_pop($operand);
+            array_shift($operand);
         }else{
-            $rpn[]=$maindata[$i];
+            array_unshift($rpn,$maindata[$i]);
         }
     }
 
-    for($i=count($operand)-1;$i>=0;$i=$i-1){
-        $rpn[]=$operand[$i];
+    for($i=0;$i<count($operand);$i++){
+        array_unshift($rpn,$operand[$i]);
     }
 
     echo(implode(" ",$rpn)."\n");
-    echo(eval("return ".$data.";").PHP_EOL);
+
+    // Calculate the result
+    // echo(str_replace('^','**',$data));
+    // echo(eval("return str_replace('^','**',$data);"));
+    echo(number_format(floor(eval("return str_replace('^','**',$data);")*1000)/1000,3,".","").PHP_EOL);
 
     $memoryafter=memory_get_usage();
     $memorydifference=$memoryafter-$memorybefore;
     echo("memory used ".($memorydifference/1048576)."MB");
 ?>
+為什麼我打 1 - 2 ^ 3
+這樣的輸出是 -4.000?
+
+而且我要的是如果有小數超過3位數才會無條件捨去，不是自動補0。
