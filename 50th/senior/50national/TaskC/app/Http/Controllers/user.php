7300588132480d9470ee283e2dee889e816dabb6
chris ho <98445729+ho0527@users.noreply.github.com>
2023-09-07 17:04:28 +0800

    new: 20230907

--- 50th/senior/50national/TaskC/app/Http/Controllers/user.php
+++ 50th/senior/50national/TaskC/app/Http/Controllers/user.php
@@ -56,11 +56,83 @@
         }
 
         public function getuser(Request $request){
-
+            $userid=logincheck();
+            if($userid){
+                if($userid=="1"){
+                    $data=[];
+                    $row=DB::table("user")
+                        ->select("*")->get();
+                    for($i=0;$i<count($row);$i=$i+1){
+                        $enabled=true;
+                        if($row[$i]->disabled=="true"){
+                            $enabled=false;
+                        }
+                        $data[]=[
+                            "id"=>$row[$i]->id,
+                            "email"=>$row[$i]->email,
+                            "nickname"=>$row[$i]->nickname,
+                            "enabled"=>$enabled,
+                            "user_type"=>$row[$i]->permission,
+                            "created_at"=>$row[$i]->createdat,
+                            "updated_at"=>$row[$i]->createdat,
+                        ];
+                    }
+                    return response()->json([
+                        "success"=>true,
+                        "message"=>"",
+                        "data"=>$data
+                    ]);
+                }else{
+                    return nopermission();
+                }
+            }else{
+                return tokenerror();
+            }
         }
 
         public function banuser(Request $request,$userid){
-
+            $loginuserid=logincheck();
+            if($loginuserid){
+                if($loginuserid=="1"){
+                    if($request->has("ban")){
+                        if($request->input("ban")){
+                            $row=DB::table("user")
+                                ->where("id","=",$userid)
+                                ->select("*")->get();
+                            if($row->isNotEmpty()){
+                                if($row[0]->disabled=="false"){
+                                    DB::table("user")
+                                        ->where("id","=",$userid)
+                                        ->update([
+                                            "disabled"=>"true",
+                                        ]);
+                                }else{
+                                    DB::table("user")
+                                        ->where("id","=",$userid)
+                                        ->update([
+                                            "disabled"=>"false",
+                                        ]);
+                                }
+                                return response()->json([
+                                    "success"=>true,
+                                    "message"=>"",
+                                    "data"=>""
+                                ]);
+                            }else{
+                                return usernotfound();
+                            }
+                        }else{
+                            return datatypeerror();
+                        }
+                    }else{
+                        return missingfield();
+                    }
+                }else{
+                    return nopermission();
+                }
+            }else{
+                return tokenerror();
+            }
         }
 
         public function getblocklist(Request $request){
