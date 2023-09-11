d58f2f5a63f6b1cdf7692f3b7f1df1e128af462a
chris ho <98445729+ho0527@users.noreply.github.com>
2023-09-11 21:50:43 +0800

    new: 20230911
    - 首頁更新

--- index.js
+++ index.js
@@ -3,117 +3,140 @@ function statetest(state){
         return "green"
     }else if(state=="FAILED"){
         return "red"
-    }else if(state=="N/A"){
+    }else if(state=="N/A"||state=="subjective"){
         return "gray"
     }else{
         return "white"
     }
 }
 
+function linktest(link,title){
+    if(link["link"]!=""&&link["link"]!=undefined&&link["state"]!="N/A"&&link["state"]!="subjective"){
+        return "<a href='"+link["link"]+"' class='a'>"+title+"</a>"
+    }else{
+        return title
+    }
+}
 
 let ajax=new XMLHttpRequest()
 
 ajax.onload=function(){
-    let data=JSON.parse(ajax.responseText)
-    for(let i=0;i<data["data"].length;i=i+1){
-        if(data["data"][i]["ps"]=="list"){
-            let title=data["data"][i]["title"]
-            let list=data["data"][i]["data"]
-            let listdata=""
-            let statecolor=""
-
+    let data=JSON.parse(ajax.responseText)["data"]
+    for(let i=0;i<data.length;i=i+1){
+        let title=data[i]["title"]
+        let list=data[i]["data"]
+        let listdata=""
+        if(data[i]["ps"]=="list"){
             for(let j=0;j<list.length;j=j+1){
-                let liststatecolor=""
-                let listlinkdata=""
-                if(list[j]["state"]=="SUCCESS"){
-                    liststatecolor="green"
-                    listlinkdata="<a href='"+list[j]["link"]+"' class='a'>"+list[j]["title"]+"</a>"
-                }else if(list[j]["state"]=="FAILED"){
-                    liststatecolor="red"
-                    listlinkdata="<a href='"+list[j]["link"]+"' class='a'>"+list[j]["title"]+"</a>"
-                }else if(list[j]["state"]=="N/A"){
-                    liststatecolor="gray"
-                    listlinkdata=list[j]["title"]
-                }else{
-                    liststatecolor="white"
-                    listlinkdata="<a href='"+list[j]["link"]+"' class='a'>"+list[j]["title"]+"</a>"
+                let listdata2=""
+                for(let k=0;k<list[j]["data"].length;k=k+1){
+                    let questiondata=""
+                    let question=list[j]["data"][k]["data"]
+
+                    for(let l=0;l<question.length;l=l+1){
+                        let descriptiondata=""
+                        console.log(question[l])
+                        for(let m=0;m<question[l]["data"].length;m=m+1){
+                            descriptiondata=descriptiondata+`
+                                <div class="descriptiondiv">
+                                    <div class="no">-</div>
+                                    <div class="title m5">${question[l]["data"][m]["description"]}</div>
+                                    <div class="dot">............</div>
+                                    <div class="state" style="color: ${statetest(question[l]["data"][m]["state"])}">${question[l]["data"][m]["state"]}</div>
+                                </div>
+                            `
+                        }
+                        questiondata=questiondata+`
+                            <div class="questiondiv">
+                                <div class="no">no.${question[l]["no"]}</div>
+                                <div class="title m5">${linktest(question[l],question[l]["description"])}</div>
+                                <div class="dot">............</div>
+                                <div class="state" style="color: ${statetest(question[l]["state"])}">${question[l]["state"]}</div>
+                            </div>
+                            <div class="list3">${descriptiondata}</div>
+                        `
+                    }
+
+                    listdata2=listdata2+`
+                        <div class="listdiv">
+                            <div class="title">項目: ${list[j]["data"][k]["list"]}-${list[j]["data"][k]["description"]}</div>
+                            <div class="dot">............</div>
+                            <div class="state" style="color: ${statetest(list[j]["data"][k]["state"])}">${list[j]["data"][k]["state"]}</div>
+                        </div>
+                        <div class="list2">${questiondata}</div>
+                    `
                 }
+
                 listdata=listdata+`
                     <div class="listdiv">
-                        <div class="title">${listlinkdata}</div>
-                        ............
-                        <div class="state" style="color: ${liststatecolor}">${list[j]["state"]}</div>
+                        <div class="title">${linktest(list[j],list[j]["title"])}</div>
+                        <div class="dot">............</div>
+                        <div class="state" style="color: ${statetest(list[j]["state"])}">${list[j]["state"]}</div>
                         <div class="cdate">createdate: ${list[j]["createdate"]}</div>
                         <div class="udate">updatedate: ${list[j]["updatedate"]}</div>
                         <div class="edate">enddate: ${list[j]["enddate"]}</div>
                     </div>
+                    <div class="list2">${listdata2}</div>
                 `
             }
-
-            document.getElementById("body").innerHTML=document.getElementById("body").innerHTML+`
-                <div class="subjectdiv">
-                    <div class="subject">
-                        <div class="title"><a href="${data["data"][i]["link"]}" class="a">${title}</a></div>
-                        .......
-                        <div class="state" style="color: ${statetest(data["data"][i]["state"])}">${data["data"][i]["state"]}</div>
-                        <div class="cdate">createdate: ${data["data"][i]["createdate"]}</div>
-                        <div class="udate">updatedate: ${data["data"][i]["updatedate"]}</div>
-                        <div class="edate">enddate: ${data["data"][i]["enddate"]}</div>
-                    </div>
-                    <div class="list">${listdata}</div>
-                <div>
-            `
-        }else if(data["data"][i]["ps"]=="question"){
-            let title=data["data"][i]["title"]
-            let list=data["data"][i]["data"]
-            let listdata=""
-
+        }else if(data[i]["ps"]=="question"){
             for(let j=0;j<list.length;j=j+1){
                 let questiondata=""
 
-                console.log(list[j]["data"])
                 for(let k=0;k<list[j]["data"].length;k=k+1){
                     let question=list[j]["data"]
+                    let descriptiondata=""
+
+                    for(let l=0;l<question[k]["data"].length;l=l+1){
+                        descriptiondata=descriptiondata+`
+                            <div class="descriptiondiv">
+                                <div class="no">-</div>
+                                <div class="title m5">${question[k]["description"]}</div>
+                                <div class="dot">............</div>
+                                <div class="state" style="color: ${statetest(question[k]["state"])}">${question[k]["state"]}</div>
+                            </div>
+                        `
+                    }
 
                     questiondata=questiondata+`
                         <div class="questiondiv">
                             <div class="no">no.${question[k]["no"]}</div>
                             <div class="title m5">${question[k]["description"]}</div>
-                            ............
+                            <div class="dot">............</div>
                             <div class="state" style="color: ${statetest(question[k]["state"])}">${question[k]["state"]}</div>
                         </div>
+                        <div class="list3">${descriptiondata}</div>
                     `
                 }
 
                 listdata=listdata+`
                     <div class="listdiv">
                         <div class="title">項目: ${list[j]["list"]}-${list[j]["description"]}</div>
-                        ............
+                        <div class="dot">............</div>
                         <div class="state" style="color: ${statetest(list[j]["state"])}">${list[j]["state"]}</div>
                     </div>
-                    <div class="list2">
-                        ${questiondata}
-                    </div>
+                    <div class="list2">${questiondata}</div>
                 `
             }
-
-            document.getElementById("body").innerHTML=document.getElementById("body").innerHTML+`
-                <div class="subjectdiv">
-                    <div class="subject">
-                        <div class="title"><a href="${data["data"][i]["link"]}" class="a">${title}</a></div>
-                        .......
-                        <div class="state" style="color: ${statetest(data["data"][i]["state"])}">${data["data"][i]["state"]}</div>
-                        <div class="cdate">createdate: ${data["data"][i]["createdate"]}</div>
-                        <div class="udate">updatedate: ${data["data"][i]["updatedate"]}</div>
-                        <div class="edate">enddate: ${data["data"][i]["enddate"]}</div>
-                    </div>
-                    <div class="list">${listdata}</div>
-                <div>
-            `
         }
 
+        document.getElementById("body").innerHTML=document.getElementById("body").innerHTML+`
+            <div class="subjectdiv">
+                <div class="subject">
+                    <div class="title">${linktest(data[i],title)}</div>
+                    .......
+                    <div class="state" style="color: ${statetest(data[i]["state"])}">${data[i]["state"]}</div>
+                    <div class="cdate">createdate: ${data[i]["createdate"]}</div>
+                    <div class="udate">updatedate: ${data[i]["updatedate"]}</div>
+                    <div class="edate">enddate: ${data[i]["enddate"]}</div>
+                </div>
+                <div class="list1">${listdata}</div>
+            <div>
+        `
     }
 }
 
 ajax.open("GET","list.json")
 ajax.send()
+
+startmacossection()
---
+++ usemyself.js
@@ -0,0 +1,119 @@
+function statetest(state){
+    if(state=="SUCCESS"){
+        return "green"
+    }else if(state=="FAILED"){
+        return "red"
+    }else if(state=="N/A"){
+        return "gray"
+    }else{
+        return "white"
+    }
+}
+
+
+let ajax=new XMLHttpRequest()
+
+ajax.onload=function(){
+    let data=JSON.parse(ajax.responseText)
+    for(let i=0;i<data["data"].length;i=i+1){
+        if(data["data"][i]["ps"]=="list"){
+            let title=data["data"][i]["title"]
+            let list=data["data"][i]["data"]
+            let listdata=""
+            let statecolor=""
+
+            for(let j=0;j<list.length;j=j+1){
+                let liststatecolor=""
+                let listlinkdata=""
+                if(list[j]["state"]=="SUCCESS"){
+                    liststatecolor="green"
+                    listlinkdata="<a href='"+list[j]["link"]+"' class='a'>"+list[j]["title"]+"</a>"
+                }else if(list[j]["state"]=="FAILED"){
+                    liststatecolor="red"
+                    listlinkdata="<a href='"+list[j]["link"]+"' class='a'>"+list[j]["title"]+"</a>"
+                }else if(list[j]["state"]=="N/A"){
+                    liststatecolor="gray"
+                    listlinkdata=list[j]["title"]
+                }else{
+                    liststatecolor="white"
+                    listlinkdata="<a href='"+list[j]["link"]+"' class='a'>"+list[j]["title"]+"</a>"
+                }
+                listdata=listdata+`
+                    <div class="listdiv">
+                        <div class="title">${listlinkdata}</div>
+                        ............
+                        <div class="state" style="color: ${liststatecolor}">${list[j]["state"]}</div>
+                        <div class="cdate">createdate: ${list[j]["createdate"]}</div>
+                        <div class="udate">updatedate: ${list[j]["updatedate"]}</div>
+                        <div class="edate">enddate: ${list[j]["enddate"]}</div>
+                    </div>
+                `
+            }
+
+            document.getElementById("body").innerHTML=document.getElementById("body").innerHTML+`
+                <div class="subjectdiv">
+                    <div class="subject">
+                        <div class="title"><a href="${data["data"][i]["link"]}" class="a">${title}</a></div>
+                        .......
+                        <div class="state" style="color: ${statetest(data["data"][i]["state"])}">${data["data"][i]["state"]}</div>
+                        <div class="cdate">createdate: ${data["data"][i]["createdate"]}</div>
+                        <div class="udate">updatedate: ${data["data"][i]["updatedate"]}</div>
+                        <div class="edate">enddate: ${data["data"][i]["enddate"]}</div>
+                    </div>
+                    <div class="list">${listdata}</div>
+                <div>
+            `
+        }else if(data["data"][i]["ps"]=="question"){
+            let title=data["data"][i]["title"]
+            let list=data["data"][i]["data"]
+            let listdata=""
+
+            for(let j=0;j<list.length;j=j+1){
+                let questiondata=""
+
+                console.log(list[j]["data"])
+                for(let k=0;k<list[j]["data"].length;k=k+1){
+                    let question=list[j]["data"]
+
+                    questiondata=questiondata+`
+                        <div class="questiondiv">
+                            <div class="no">no.${question[k]["no"]}</div>
+                            <div class="title m5">${question[k]["description"]}</div>
+                            ............
+                            <div class="state" style="color: ${statetest(question[k]["state"])}">${question[k]["state"]}</div>
+                        </div>
+                    `
+                }
+
+                listdata=listdata+`
+                    <div class="listdiv">
+                        <div class="title">項目: ${list[j]["list"]}-${list[j]["description"]}</div>
+                        ............
+                        <div class="state" style="color: ${statetest(list[j]["state"])}">${list[j]["state"]}</div>
+                    </div>
+                    <div class="list2">
+                        ${questiondata}
+                    </div>
+                `
+            }
+
+            document.getElementById("body").innerHTML=document.getElementById("body").innerHTML+`
+                <div class="subjectdiv">
+                    <div class="subject">
+                        <div class="title"><a href="${data["data"][i]["link"]}" class="a">${title}</a></div>
+                        .......
+                        <div class="state" style="color: ${statetest(data["data"][i]["state"])}">${data["data"][i]["state"]}</div>
+                        <div class="cdate">createdate: ${data["data"][i]["createdate"]}</div>
+                        <div class="udate">updatedate: ${data["data"][i]["updatedate"]}</div>
+                        <div class="edate">enddate: ${data["data"][i]["enddate"]}</div>
+                    </div>
+                    <div class="list">${listdata}</div>
+                <div>
+            `
+        }
+
+    }
+}
+
+ajax.open("GET","listusemyself.json")
+ajax.send()
