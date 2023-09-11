43bb9eeb60fdc86717c6c9312b1c2cb9b2bbbe32
chris ho <98445729+ho0527@users.noreply.github.com>
2023-09-11 17:44:08 +0800

    new: 20230911
    - 首頁更新

--- anther/plugin/chrisplugin/chrisplugin.js
+++ anther/plugin/chrisplugin/chrisplugin.js
@@ -27,278 +27,278 @@
     |-------    -----    -     -    -          -     -----         -----  -------|
 */
 
-function startmacossection(){ // start macos section
+function startmacossection() { // start macos section
     let macostimer
-    setTimeout(function(){
-        document.querySelectorAll(".macossectiondiv").forEach(function(event){
-            event.addEventListener("scroll",function(){
+    setTimeout(function () {
+        document.querySelectorAll(".macossectiondiv").forEach(function (event) {
+            event.addEventListener("scroll", function () {
                 clearTimeout(macostimer)
-                event.setAttribute("scroll","true")
-                macostimer=setTimeout(function(){
+                event.setAttribute("scroll", "true")
+                macostimer = setTimeout(function () {
                     event.removeAttribute("scroll")
-                },500)
+                }, 500)
             })
         })
-        document.querySelectorAll(".macossectiondivx").forEach(function(event){
-            event.addEventListener("scroll",function(){
+        document.querySelectorAll(".macossectiondivx").forEach(function (event) {
+            event.addEventListener("scroll", function () {
                 clearTimeout(macostimer)
-                event.setAttribute("scroll","true")
-                macostimer=setTimeout(function(){
+                event.setAttribute("scroll", "true")
+                macostimer = setTimeout(function () {
                     event.removeAttribute("scroll")
-                },500)
+                }, 500)
             })
         })
-        document.querySelectorAll(".macossectiondivy").forEach(function(event){
-            event.addEventListener("scroll",function(){
+        document.querySelectorAll(".macossectiondivy").forEach(function (event) {
+            event.addEventListener("scroll", function () {
                 clearTimeout(macostimer)
-                event.setAttribute("scroll","true")
-                macostimer=setTimeout(function(){
+                event.setAttribute("scroll", "true")
+                macostimer = setTimeout(function () {
                     event.removeAttribute("scroll")
-                },500)
+                }, 500)
             })
         })
-    },200)
+    }, 200)
 }
 
-function divsort(card,sortdiv){ // card 放要被拖的物件 cardclass(不加選擇器) sortdiv 放要放的物件(加選擇器)
-    let data=[]
+function divsort(card, sortdiv) { // card 放要被拖的物件 cardclass(不加選擇器) sortdiv 放要放的物件(加選擇器)
+    let data = []
 
-    document.querySelectorAll("."+card).forEach(function(event){
-        event.draggable="true"
+    document.querySelectorAll("." + card).forEach(function (event) {
+        event.draggable = "true"
     })
 
-    document.querySelectorAll(sortdiv).forEach(function(event){
-        event.ondragstart=function(addeventlistenerevent){
+    document.querySelectorAll(sortdiv).forEach(function (event) {
+        event.ondragstart = function (addeventlistenerevent) {
             addeventlistenerevent.target.classList.add("divsortdragging")
         }
 
-        event.ondragover=function(addeventlistenerevent){
+        event.ondragover = function (addeventlistenerevent) {
             addeventlistenerevent.preventDefault()
-            let sortableContainer=addeventlistenerevent.target.closest(sortdiv)
-            if(sortableContainer){
-                let draggableElements=Array.from(sortableContainer.children).filter(function(child){
-                    return child.classList.contains(card)&&!child.classList.contains("divsortdragging")
+            let sortableContainer = addeventlistenerevent.target.closest(sortdiv)
+            if (sortableContainer) {
+                let draggableElements = Array.from(sortableContainer.children).filter(function (child) {
+                    return child.classList.contains(card) && !child.classList.contains("divsortdragging")
                 })
 
-                let afterElement=draggableElements.reduce(function(closest,child){
-                    let box=child.getBoundingClientRect()
-                    let offset=addeventlistenerevent.clientY-box.top-box.height/2
-                    if(offset<0&&offset>closest.offset){
-                        return { offset:offset,element:child }
-                    }else{
+                let afterElement = draggableElements.reduce(function (closest, child) {
+                    let box = child.getBoundingClientRect()
+                    let offset = addeventlistenerevent.clientY - box.top - box.height / 2
+                    if (offset < 0 && offset > closest.offset) {
+                        return { offset: offset, element: child }
+                    } else {
                         return closest
                     }
-                },{ offset:Number.NEGATIVE_INFINITY }).element
+                }, { offset: Number.NEGATIVE_INFINITY }).element
 
-                let draggable=document.querySelector(".divsortdragging")
-                if(afterElement==null){
+                let draggable = document.querySelector(".divsortdragging")
+                if (afterElement == null) {
                     sortableContainer.appendChild(draggable)
-                }else{
-                    sortableContainer.insertBefore(draggable,afterElement)
+                } else {
+                    sortableContainer.insertBefore(draggable, afterElement)
                 }
             }
         }
 
-        event.ondragend=function(addeventlistenerevent){
+        event.ondragend = function (addeventlistenerevent) {
             addeventlistenerevent.target.classList.remove("divsortdragging")
         }
     })
 
-    document.querySelectorAll(card).forEach(function(event){
+    document.querySelectorAll(card).forEach(function (event) {
         data.push(event)
     })
 
     return data
 }
 
-function docget(key,selector){ // 傳入 id animation class name tag tagns qtor all 分別對應為 getElementById getAnimations getElementsByClassName getElementsByName getElementsByTagName getElementsByTagNameNS querySelector querySelectorAll 之後傳入要執行的selector
-    if(key=="id"){
+function docget(key, selector) { // 傳入 id animation class name tag tagns qtor all 分別對應為 getElementById getAnimations getElementsByClassName getElementsByName getElementsByTagName getElementsByTagNameNS querySelector querySelectorAll 之後傳入要執行的selector
+    if (key == "id") {
         return document.getElementById(selector)
     }
-    if(key=="animation"){
+    if (key == "animation") {
         return document.getAnimations(selector)
     }
-    if(key=="class"){
+    if (key == "class") {
         return document.getElementsByClassName(selector)
     }
-    if(key=="name"){
+    if (key == "name") {
         return document.getElementsByName(selector)
     }
-    if(key=="tag"){
+    if (key == "tag") {
         return document.getElementsByTagName(selector)
     }
-    if(key=="tagns"){
+    if (key == "tagns") {
         return document.getElementsByTagNameNS(selector)
     }
-    if(key=="qtor"){
+    if (key == "qtor") {
         return document.querySelector(selector)
     }
-    if(key=="all"){
+    if (key == "all") {
         return document.querySelectorAll(selector)
     }
-    conlog("[ERROR]dget key not found"+key)
+    conlog("[ERROR]dget key not found" + key)
 }
 
-function docgetid(selector){ // document.getElementById
+function docgetid(selector) { // document.getElementById
     return document.getElementById(selector)
 }
 
-function docgetall(selector){ // document.querySelectorAll
+function docgetall(selector) { // document.querySelectorAll
     return document.querySelectorAll(selector)
 }
 
-function conlog(data,color="white",size="12",weight="normal"){ // consloe.log 值出來
-    console.log(`%c${data}`,`color:${color};font-size:${size}px;font-weight:${weight}`)
+function conlog(data, color = "white", size = "12", weight = "normal") { // consloe.log 值出來
+    console.log(`%c${data}`, `color:${color};font-size:${size}px;font-weight:${weight}`)
 }
 
-function isset(data){ // 看值是否存在
-    if(data!=null||data!=undefined){ return true }
-    else{ return false }
+function isset(data) { // 看值是否存在
+    if (data != null || data != undefined) { return true }
+    else { return false }
 }
 
-function blank(data){ // 看值是否不為空
-    if(data!=null||data!=undefined||data!=""){ return true }
-    else{ return false }
+function blank(data) { // 看值是否不為空
+    if (data != null || data != undefined || data != "") { return true }
+    else { return false }
 }
 
-function weblsset(data,value){ // localStorage.setItem
-    return localStorage.setItem(data,value)
+function weblsset(data, value) { // localStorage.setItem
+    return localStorage.setItem(data, value)
 }
 
-function weblsget(data){ // localStorage.getItem
+function weblsget(data) { // localStorage.getItem
     return localStorage.getItem(data)
 }
 
-function doccreate(element){ // document.createElement
+function doccreate(element) { // document.createElement
     return document.createElement(element)
 }
 
-function newajax(method,url,send=null){
-    let ajax=new XMLHttpRequest()
+function newajax(method, url, send = null) {
+    let ajax = new XMLHttpRequest()
 
-    ajax.open(method,url)
+    ajax.open(method, url)
     ajax.send(send)
 
     return ajax
 }
 
-function lightbox(clickelement,element,lightboxhtml,closelement=null,islightboxclosewithkeyesc=true){
+function lightbox(clickelement, element, lightboxhtml, closelement = null, islightboxclosewithkeyesc = true) {
     docgetid(element).classList.add("lightboxmask")
 
-    setTimeout(function(){
-        if(clickelement==null){
-            docgetid(element).innerHTML=``
-            docgetid(element).style.display="block"
-            setTimeout(function(){
-                docgetid(element).style.transform="translateY(0)"
-            },10)
-            html=`
+    setTimeout(function () {
+        if (clickelement == null) {
+            docgetid(element).innerHTML = ``
+            docgetid(element).style.display = "block"
+            setTimeout(function () {
+                docgetid(element).style.transform = "translateY(0)"
+            }, 10)
+            html = `
                 <div class="lightboxmain macossectiondiv">
                     ${lightboxhtml()}
                 </div>
             `
-            docgetid(element).innerHTML=html
-            if(closelement!=null){
-                docgetid(closelement).onclick=function(){
-                    docgetid(element).style.transform="translateY(-100%)"
+            docgetid(element).innerHTML = html
+            if (closelement != null) {
+                docgetid(closelement).onclick = function () {
+                    docgetid(element).style.transform = "translateY(-100%)"
 
-                    setTimeout(function(){
-                        docgetid(element).style.display="none"
-                        docgetid(element).innerHTML=``
-                    },300)
+                    setTimeout(function () {
+                        docgetid(element).style.display = "none"
+                        docgetid(element).innerHTML = ``
+                    }, 300)
                 }
             }
-        }else{
-            docgetid(element).innerHTML=``
-            docgetall(clickelement).forEach(function(event){
-                event.onclick=function(){
-                    docgetid(element).style.display="block"
-                    setTimeout(function(){
-                        docgetid(element).style.transform="translateY(0)"
-                    },10)
-                    html=`
+        } else {
+            docgetid(element).innerHTML = ``
+            docgetall(clickelement).forEach(function (event) {
+                event.onclick = function () {
+                    docgetid(element).style.display = "block"
+                    setTimeout(function () {
+                        docgetid(element).style.transform = "translateY(0)"
+                    }, 10)
+                    html = `
                         <div class="lightboxmain macossectiondiv">
-                            `+lightboxhtml(event)+`
+                            `+ lightboxhtml(event) + `
                         </div>
                     `
-                    docgetid(element).innerHTML=html
-                    if(closelement!=null){
-                        docgetid(closelement).onclick=function(){
-                            docgetid(element).style.transform="translateY(-100%)"
+                    docgetid(element).innerHTML = html
+                    if (closelement != null) {
+                        docgetid(closelement).onclick = function () {
+                            docgetid(element).style.transform = "translateY(-100%)"
 
-                            setTimeout(function(){
-                                docgetid(element).style.display="none"
-                                docgetid(element).innerHTML=``
-                            },300)
+                            setTimeout(function () {
+                                docgetid(element).style.display = "none"
+                                docgetid(element).innerHTML = ``
+                            }, 300)
                         }
                     }
                 }
             })
         }
 
-        if(islightboxclosewithkeyesc){
-            document.addEventListener("keydown",function(event){
-                if(event.key=="Escape"){
+        if (islightboxclosewithkeyesc) {
+            document.addEventListener("keydown", function (event) {
+                if (event.key == "Escape") {
                     event.preventDefault()
-                    docgetid(element).style.transform="translateY(-100%)"
+                    docgetid(element).style.transform = "translateY(-100%)"
 
-                    setTimeout(function() {
-                        docgetid(element).style.display="none"
-                        docgetid(element).innerHTML=``
-                    },300)
+                    setTimeout(function () {
+                        docgetid(element).style.display = "none"
+                        docgetid(element).innerHTML = ``
+                    }, 300)
                 }
             })
         }
-    },70)
+    }, 70)
 }
 
-function docappendchild(element,chlidelement){
+function docappendchild(element, chlidelement) {
     return docgetid(element).appendChild(chlidelement)
 }
 
-function regexp(regexptext,regexpstring){
-    return new RegExp(regexptext,regexpstring)
+function regexp(regexptext, regexpstring) {
+    return new RegExp(regexptext, regexpstring)
 }
 
-function regexpmatch(data,regexptext,regexpstring=""){
-    return data.match(regexp(regexptext,regexpstring))
+function regexpmatch(data, regexptext, regexpstring = "") {
+    return data.match(regexp(regexptext, regexpstring))
 }
 
-function regexpreplace(data,replacetext,regexptext,regexpstring=""){
-    return data.replace(regexp(regexptext,regexpstring),replacetext)
+function regexpreplace(data, replacetext, regexptext, regexpstring = "") {
+    return data.replace(regexp(regexptext, regexpstring), replacetext)
 }
 
-function formdata(data=[]){
-    let formdata=new FormData()
-    for(let i=0;i<data.length;i=i+1){
-        formdata.append(data[i][0],data[i][1])
+function formdata(data = []) {
+    let formdata = new FormData()
+    for (let i = 0; i < data.length; i = i + 1) {
+        formdata.append(data[i][0], data[i][1])
     }
     return formdata
 }
 
-function pagechanger(data,ipp,key,callback){
-    if(!isset(weblsget("pagecount"))){ weblsset("pagecount",1) }
-    let row=data
-    let pagecount=parseInt(weblsget("pagecount"))
-    let itemperpage=ipp
-    let maxpagecount=Math.ceil(row.length/itemperpage)
-    if(key=="first"){
-        pagecount=1
-    }else if(key=="prev"){
-        pagecount=Math.max(1,pagecount-1)
-    }else if(key=="next"){
-        pagecount=Math.min(pagecount+1,maxpagecount)
-    }else if(key=="end"){
-        pagecount=maxpagecount
+function pagechanger(data, ipp, key, callback) {
+    if (!isset(weblsget("pagecount"))) { weblsset("pagecount", 1) }
+    let row = data
+    let pagecount = parseInt(weblsget("pagecount"))
+    let itemperpage = ipp
+    let maxpagecount = Math.ceil(row.length / itemperpage)
+    if (key == "first") {
+        pagecount = 1
+    } else if (key == "prev") {
+        pagecount = Math.max(1, pagecount - 1)
+    } else if (key == "next") {
+        pagecount = Math.min(pagecount + 1, maxpagecount)
+    } else if (key == "end") {
+        pagecount = maxpagecount
     }
 
-    weblsset("pagecount",pagecount)
-    let page=parseInt(weblsget("pagecount"))
-    let start=(page-1)*itemperpage;
-    let rowcount=Math.min(row.length-start,itemperpage);
-    let end=start+rowcount;
+    weblsset("pagecount", pagecount)
+    let page = parseInt(weblsget("pagecount"))
+    let start = (page - 1) * itemperpage;
+    let rowcount = Math.min(row.length - start, itemperpage);
+    let end = start + rowcount;
 
-    for(let i=start;i<end;i=i+1){
+    for (let i = start; i < end; i = i + 1) {
         callback(row[i])
     }
 }
