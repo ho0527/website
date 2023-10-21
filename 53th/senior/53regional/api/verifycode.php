b013ccc525554d40d7870773b93ee8a0b30ab45b
chris ho <98445729+ho0527@users.noreply.github.com>
2023-10-21 23:46:16 +0800

    docs: 20231021
    - 更新程式碼(加註解)

--- 53th/senior/53regional/api/verifycode.php
+++ 53th/senior/53regional/api/verifycode.php
@@ -1,11 +1,11 @@
 <?php
-    $str=$_GET["str"];
-    $canva=imagecreate(50,50);
-    imagecolorallocate($canva,255,255,255);
-    $paint=imagecolorallocate($canva,0,0,0);
-    $font="font.TTF";
-    imagettftext($canva,30,0,15,40,$paint,$font,$str);
-    header("content-type:image/png");
-    imagepng($canva);
-    imagedestroy($canva);
+    $str=$_GET["str"]; // 拿到資料(文字)
+    $canva=imagecreate(50,50); // 創建畫布
+    imagecolorallocate($canva,255,255,255); // 背景
+    $paint=imagecolorallocate($canva,0,0,0); // 筆的顏色
+    $font="font.TTF"; // 字型
+    imagettftext($canva,30,0,15,40,$paint,$font,$str); // 設定 大小,旋轉角,x偏移,y偏移,畫筆,字型,文字
+    header("content-type:image/png"); // 設定content-type(可加可不加)
+    imagepng($canva); // 設定成png
+    imagedestroy($canva); // 清除(可加可不加)
 ?>
