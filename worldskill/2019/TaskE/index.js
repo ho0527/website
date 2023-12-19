d530f434566b3ba616c5f0c9520cd7cbbf5d22bb
chris ho <98445729+ho0527@users.noreply.github.com>
2023-12-19 15:52:20 +0800

    new: 20231219
    - ws2019ME製作

--- worldskill/2019/TaskE/index.js
+++ worldskill/2019/TaskE/index.js
@@ -1,38 +1,85 @@
-docgetid("main").innerHTML=`
-    <div class="elementdiv" id="0" style="position: absolute;top: 50%;left: 50%;transform: translate(-50%,-50%);">
-        <div class="elementposition">
-            <div class="element"></div>
+let midx=window.innerWidth/2-60
+let midy=window.innerHeight/2-60
+let maininnerhtml=``
+let data={}
+
+if(!weblsget("worldskill2022ME")){
+    weblsset("worldskill2022ME",JSON.stringify({
+        "data": [
+            {
+                "id": 0,
+                "position": {
+                    "top": midy,
+                    "left": midx
+                },
+                "content": "",
+                "1": "",
+                "2": "",
+                "3": "",
+                "4": ""
+            }
+        ]
+    }))
+}
+data=JSON.parse(weblsget("worldskill2022ME"))
+
+// 初始化 START
+for(let i=0;i<data["data"].length;i=i+1){
+    maininnerhtml=`
+        ${maininnerhtml}
+        <div class="elementdiv" id="${data["data"][i]["position"]["top"]}" style="position: absolute;top: ${data["data"][i]["position"]["top"]}px;left: ${data["data"][i]["position"]["left"]}px;transform: ${data["data"][i]["position"]["transform"]};">
+            <div class="elementposition">
+                <div class="element"></div>
+            </div>
         </div>
-    </div>
-` // 初始化
+    `
+}
+docgetid("main").innerHTML=maininnerhtml
+// 初始化 END
 
 docgetall(".elementdiv").forEach(function(event){
     // hover時顯示
     event.onmouseover=function(){
         event.querySelectorAll(".element")[0].innerHTML=`
             <div class="elementposition2">
-                <div class="element1" id="1"><div class="element1text">1</div></div>
-                <div class="element2" id="2"><div class="element2text">2</div></div>
-                <div class="element3" id="3"><div class="element3text">3</div></div>
-                <div class="element4" id="4"><div class="element4text">4</div></div>
+                <div class="element1" id="element1"><div class="element1text">1</div></div>
+                <div class="element2" id="element2"><div class="element2text">2</div></div>
+                <div class="element3" id="element3"><div class="element3text">3</div></div>
+                <div class="element4" id="element4"><div class="element4text">4</div></div>
+                <input type="button" class="elementedit" id="edit" data-id="${event.id}" value="E">
+                <input type="button" class="elementdelete" id="delete" data-id="${event.id}" value="X">
             </div>
         `
 
         // 各元素創建 START
-        docgetid("1").onclick=function(){
+        docgetid("element1").onclick=function(){
 
         }
 
-        docgetid("2").onclick=function(){
+        docgetid("element2").onclick=function(){
+            console.log("inininin")
+        }
+
+        docgetid("element3").onclick=function(){
 
         }
 
-        docgetid("3").onclick=function(){
+        docgetid("element4").onclick=function(){
 
         }
 
-        docgetid("4").onclick=function(){
+        docgetid("edit").onclick=function(){
 
+        }
+
+        docgetid("delete").onclick=function(){
+            console.log("inininininininin")
+            docgetid(this.dataset.id).remove()
+            for(let i=0;i<data["data"].length;i=i+1){
+                if(data["data"][i]["id"]==this.dataset.id){
+                    delete data["data"][i]
+                }
+            }
         }
         // 各元素創建 END
     }
